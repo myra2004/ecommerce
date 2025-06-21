e22744a499f1007afbb83a0f683a6eca3075b466
myra2004 <amirovamuqaddas@gmail.com>
2025-06-21 17:01:46 +0500

    signals changed

--- common/context_processor.py
+++ common/context_processor.py
@@ -1,17 +1,21 @@
 from django.db import models
-
 from accounts.models import CartItem
 
-
 def common_context(request):
-    cart_items = CartItem.objects.filter(cart=request.user.cart).annotate(
-        total_amount=models.F('quantity') * models.F('product__price')
-    )
-    total_amount = sum(item.total_amount for item in cart_items)
+    cart_items = []
+    total_amount = 0
+    cart_items_count = 0
+
+    if request.user.is_authenticated and hasattr(request.user, 'cart'):
+        cart_items = CartItem.objects.filter(cart=request.user.cart).annotate(
+            total_amount=models.F('quantity') * models.F('product__price')
+        )
+        total_amount = sum(item.total_amount for item in cart_items)
+        cart_items_count = request.user.cart.cart_items.count()
 
     return {
         'site_name': 'ECommerce',
         'is_user_authenticated': request.user.is_authenticated,
-        'user_cart_items_count': request.user.cart.cart_items.count(),
+        'user_cart_items_count': cart_items_count,
         'cart_total_amount': total_amount // 100
     }
